635f23aee8a95dc95d1f7bfb7203782e82df6937
Ivan Deschenaux <ivan.deschenaux@gmail.com>
2022-12-20 11:59:03 +0100

    solve day 2 second star

--- day3/solution.R
+++ day3/solution.R
@@ -1,6 +1,7 @@
 library(tidyverse)
-
 input <- as_tibble(read_lines("day3/input.txt"))
+
+## part 1
 
 let <- c(letters, LETTERS)
 
@@ -23,3 +24,21 @@ d <- input %>%
 
 # answer
 sum(d$priority) #8394
+
+## part two
+d$value[1:3]
+
+badges <- c()
+for (i in seq(1, 298, by = 3)) {
+  elves <- d$value[i:(i+2)]
+  split <- str_split(elves, "")
+  badges <- c(badges, Reduce(intersect, split) %>% print())
+}
+
+d2 <- tibble(badges) %>%
+  rowwise() %>%
+  mutate(
+    priority = which(let == badges)
+  )
+
+sum(d2$priority) #2413
