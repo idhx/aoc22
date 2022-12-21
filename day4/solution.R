78dcfcd68679d982838d4bbb6868019aa280e3cc
Ivan Deschenaux <ivan.deschenaux@gmail.com>
2022-12-21 21:40:53 +0100

    solve day 4 second star

--- day4/solution.R
+++ day4/solution.R
@@ -13,8 +13,10 @@ d <- input %>%
   ) %>%
   select(!e1 & !e2)
 
+## part 1
+
 # function to check whether either range is contained in the other
-test <- function(x) {
+contains <- function(x) {
   seq1 <- seq(x[1], x[2])
   seq2 <- seq(x[3], x[4])
   int <- intersect(seq1, seq2)
@@ -25,6 +27,24 @@ test <- function(x) {
   }
 }
 
-d$full_overlap <- apply(X = d, MARGIN = 1, FUN = test)
+d$full_overlap <- apply(X = d, MARGIN = 1, FUN = contains)
 
 sum(d$full_overlap) # 498
+
+## part 2
+
+# function to check whether there is any overlap
+overlaps <- function(x) {
+  seq1 <- seq(x[1], x[2])
+  seq2 <- seq(x[3], x[4])
+  int <- intersect(seq1, seq2)
+  if (length(int > 0)) {
+    return(TRUE)
+  } else {
+    return(FALSE)
+  }
+}
+
+d$any_overlap <- apply(X = d, MARGIN = 1, FUN = overlaps)
+
+sum(d$any_overlap) # 859
