99932338378889156a8dd77105f1e2473fad1df1
Ivan Deschenaux <ivan.deschenaux@gmail.com>
2022-12-24 00:16:27 +0100

    solve day 6 second star

--- day6/solution.R
+++ day6/solution.R
@@ -16,3 +16,24 @@ while (!sig_found) {
   pos <- pos + 1
   }
 }
+
+# answer
+pos
+
+## part 2
+## There is probably a more efficient way of doing this
+sig_found <- FALSE
+pos <- 14
+
+while (!sig_found) {
+  window <- seq((pos - 13), pos)
+  chars <- input[window]
+  if (length(unique(chars)) == length(chars)){
+    sig_found <- TRUE
+  } else{
+  pos <- pos + 1
+  }
+}
+
+# answer
+pos
