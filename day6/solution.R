ca27c32ff4ed43c6ef24d4ef1f7a2c5f7dd685a0
Ivan Deschenaux <ivan.deschenaux@gmail.com>
2022-12-24 00:13:16 +0100

    solve day 6 first star

---
+++ day6/solution.R
@@ -0,0 +1,18 @@
+input <- readLines("day6/input.txt")
+input <- unlist(strsplit(input, ""))
+
+
+## part 1
+## There is probably a more efficient way of doing this
+sig_found <- FALSE
+pos <- 4
+
+while (!sig_found) {
+  window <- seq((pos - 3), pos)
+  chars <- input[window]
+  if (length(unique(chars)) == length(chars)){
+    sig_found <- TRUE
+  } else{
+  pos <- pos + 1
+  }
+}
